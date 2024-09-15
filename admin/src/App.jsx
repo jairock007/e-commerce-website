affb53042612ddb237493b8cca8debef01ff458b
jairock007 <jairock7776@gmail.com>
2024-09-15 15:48:08 +0530

    name change

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import NavBar from "./Components/NavBar/NavBar";
+import NavBar from "./Components/NavBar/Navbar";
 import Admin from "./Pages/Admin/Admin";
 
 const App = () => {
   return (
     <div>
-      <NavBar />
+      <Navbar />
       <Admin />
     </div>
   );
