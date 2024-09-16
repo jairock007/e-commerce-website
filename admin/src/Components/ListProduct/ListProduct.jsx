cd9a68cfe13773062413b1f2bef3aad905e5b246
jairock007 <jairock7776@gmail.com>
2024-09-16 20:28:47 +0530

    update AddProduct.jsx

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -6,7 +6,7 @@ const ListProduct = () => {
   const [allproducts, setAllProducts] = useState([]);
 
   const fetchInfo = async () => {
-    await fetch("http://localhost:4000/allproducts")
+    await fetch("https://e-commerce-website-u9x5.vercel.app/allproducts")
       .then((res) => res.json())
       .then((data) => {
         setAllProducts(data);
@@ -18,7 +18,7 @@ const ListProduct = () => {
   }, []);
 
   const remove_product = async (id) => {
-    await fetch("http://localhost:4000/removeproduct", {
+    await fetch("https://e-commerce-website-u9x5.vercel.app/removeproduct", {
       method: "POST",
       headers: {
         Accept: "application/json",
