dab49bfb664206d82a5ae5dde8ddb70c43d4707c
jairock007 <jairock7776@gmail.com>
2024-09-16 20:49:07 +0530

    Update loginSignUp.jsx

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -16,7 +16,7 @@ const LoginSignup = () => {
   const login = async () => {
     console.log("Login Function Executed", formData);
     let responseData;
-    await fetch("http://localhost:4000/login", {
+    await fetch("https://e-commerce-website-ogbc.vercel.app/login", {
       method: "POST",
       headers: {
         Accept: "application/form-data",
@@ -38,7 +38,7 @@ const LoginSignup = () => {
   const signup = async () => {
     console.log("Signup Function Executed", formData);
     let responseData;
-    await fetch("http://localhost:4000/signup", {
+    await fetch("https://e-commerce-website-ogbc.vercel.app/signup", {
       method: "POST",
       headers: {
         Accept: "application/form-data",
